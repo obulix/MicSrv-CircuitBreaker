a9fd0fa15d6c8e38f19cb6476879ee811fec2d04
obulix@hotmail.com <writingtogeetha@gmail.com>
2023-11-26 23:17:43 -0600

    initial checkin

---
+++ circuitbreaker.inventoryservice/src/test/java/com/dsigpattern/circuitbreaker/inventoryservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsigpattern.circuitbreaker.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
